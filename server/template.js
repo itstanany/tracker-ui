e2b4184432e38ec503b59d2ba7da822ac3d4c1f7
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-03-07 13:36:07 +0200

    template changes for styles

--- server/template.js
+++ server/template.js
@@ -18,12 +18,12 @@ export default function template(body, initialData, userData) {
     <html lang="en">
       <head>
         <meta charset="UTF-8">
+        <title>Issue Tracker</title>
+        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/style/style.css" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/style/style.css" />
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css" >
         <script src="https://apis.google.com/js/api:client.js"></script>
-        <title>Issue Tracker</title>
       </head>
 
       <body>
